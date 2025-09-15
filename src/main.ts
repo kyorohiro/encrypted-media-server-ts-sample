20ec7a057814b92aa7629bddf94cf95e95bce4c0
kyorohiro <kyorohiro@gmail.com>
2025-09-15 15:03:38 +0900

    add express helloworld and minio

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,24 @@
-const main = async () => {
-	console.log("Hello, World!!");
-};
+import express from "express"
 
-main().catch((error)=>{
-	console.error("Error occurred:", error);
+const app = express();
+
+const PORT = Number(process.env.PORT || 3000);
+
+// ヘルスチェック
+app.get("/healthz", (_req, res) => res.send("ok"));
+
+app.get("/", (req, res) => {
+	res.send("Hello, World!!");
 });
+
+app.listen(PORT, () => {
+	console.log(`Server is running on port ${PORT}`);
+});
+
+//const main = async () => {
+//	console.log("Hello, World!!");
+//};
+//
+//main().catch((error)=>{
+//	console.error("Error occurred:", error);
+//});
